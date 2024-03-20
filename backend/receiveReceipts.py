fe5029112cea6f6013e05614a3807bfb8d12583b
ranbirsharma <ranbirs>
2024-03-20 14:09:42 -0700

    Removed comments

--- backend/receiveReceipts.py
+++ backend/receiveReceipts.py
@@ -1,10 +1,8 @@
-# auth.py
-
 from flask import jsonify, request
 
-# Define the route for authentication
+
 def receiveReceipts():
-    # Get username and password from the request data
+
     data = request.json
     merchantID = data.get('merchantID')
     receipt = data.get('receipt')
